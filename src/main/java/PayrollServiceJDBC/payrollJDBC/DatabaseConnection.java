0b6e5489e37ca4f698960d371999815b3de3fdef
Ashwini Garad <ashwinigarad1041@gmail.com>
2024-09-15 16:49:13 +0530

    Welcome message

---
+++ src/main/java/PayrollServiceJDBC/payrollJDBC/DatabaseConnection.java
@@ -0,0 +1,11 @@
+package PayrollServiceJDBC.payrollJDBC;
+
+public class DatabaseConnection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Database Connection!");
+
+	}
+
+}
