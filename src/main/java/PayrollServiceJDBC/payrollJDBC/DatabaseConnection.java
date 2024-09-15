e0164fb072f85f1a51304a4e1ad7ca2a40eef6f9
Ashwini Garad <ashwinigarad1041@gmail.com>
2024-09-15 17:16:22 +0530

    Added : payroll_service database JDBC connection

--- src/main/java/PayrollServiceJDBC/payrollJDBC/DatabaseConnection.java
+++ src/main/java/PayrollServiceJDBC/payrollJDBC/DatabaseConnection.java
@@ -1,11 +1,40 @@
 package PayrollServiceJDBC.payrollJDBC;
-
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
 public class DatabaseConnection {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Database Connection!");
+	    String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+	    String userName = "root";
+	    String password = "";
+	    Connection connection;
+	    try {
+	    	Class.forName("com.mysql.cj.jdbc.Driver");
+	    	System.out.println("Driver loaded");
+	    }catch(ClassNotFoundException e) {
+	    	throw new IllegalStateException("Cannot find the driver in the classpath" , e);
+	    }
+	    listDrivers();
+	    try {
+	    	System.out.println("Connection to successful : "+ jdbcURL);
+	    	connection = DriverManager.getConnection(jdbcURL, userName, password);
+	    	System.out.println();
+	    	
+	    }catch(Exception e) {
+	    	e.printStackTrace(); 
+	    }
 
+	}
+
+	public static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while(driverList.hasMoreElements()) {
+			Driver driverClass = (Driver)driverList.nextElement();
+			System.out.println(" " + driverClass.getClass().getName() );
+		}
+		
 	}
 
 }
